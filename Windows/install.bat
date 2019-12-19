8d182f2e297a6feb6da2f6e3bb1a1bcdbb6a5fe8
“alexandro591” <alexandrotapiaflores@gmail.com>
2019-12-18 22:44:14 -0500

    improved for windows

--- Windows/install.bat
+++ Windows/install.bat
@@ -1,2 +1,6 @@
+@echo off
 cd /d %~dp0
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 type hosts.txt>C:\Windows\System32\drivers\etc\hosts
+echo Service installed
+pause
